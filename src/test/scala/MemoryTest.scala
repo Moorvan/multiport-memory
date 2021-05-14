868f91e39bb89ce7c98b3b9bf7f1d7e031fbc329
mufanx <xmfnnj@outlook.com>
2021-05-14 10:21:29 +0800

    dismiss

--- src/test/scala/MemoryTest.scala
+++ src/test/scala/MemoryTest.scala
@@ -1,27 +1,9 @@
 package memory
 import chisel3._
 import chisel3.iotesters._
-/**
- * @author YueChen
- * @date 2021/2/16 18:19
- * @version 1.0
- */
 
-class MemoryTest(dut: Memory) extends PeekPokeTester(dut){
-  poke(dut.io.wrAddr, 0.U)
-  poke(dut.io.wrData, 19.U)
-  poke(dut.io.wrEna, 1.U)
-  step(1)
-  poke(dut.io.rdAddr, 0.U)
-  step(1)
-  expect(dut.io.rdData, 19.U)
-}
 
-object MemoryTest extends App {
-  chisel3.iotesters.Driver(() => new Memory(16, 8)) {
-    c => new MemoryTest(c)
-  }
-}
+
 
 
 class XORMultiPortMemoryTest(dut: XORMultiPortMemory) extends PeekPokeTester(dut) {
